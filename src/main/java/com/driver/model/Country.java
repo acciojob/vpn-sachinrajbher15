4134390a9205689f39cb57fc94f4e8abae847cba
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-07-04 20:17:32 +0530

    Update Country.java

--- src/main/java/com/driver/model/Country.java
+++ src/main/java/com/driver/model/Country.java
@@ -2,39 +2,73 @@
 package com.driver.model;
 
 import javax.persistence.*;
-import java.security.Provider;
 
 @Entity
-@Table(name="country")
-public class Country{
+@Table(name = "country")
+public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private CountryName countryName;
+
+    private CountryName countryName; //here @Enumerated is not written bcz its instruction
+
     private String code;
 
+
+    //child w.r.t ServiceProvider
     @ManyToOne
     @JoinColumn
     private ServiceProvider serviceProvider;
 
+    //child w.r.t User
     @OneToOne
     @JoinColumn
     private User user;
 
-    //constructor
-    public Country(){}
-    //getters
-    public int getId(){ return id;}
-    public CountryName getCountryName(){ return countryName;}
-    public String getCode(){ return code;}
-    //setters
-    public void setId(int id){ this.id = id;}
-    public void setCountryName(CountryName countryName){ this.countryName = countryName;}
-    public void setCode(String code) { this.code = code;}
 
-    public void setUser(User user) { this.user = user;}
-    public User getUser(){ return user; }
+    //constructor, getters, setters
 
-    public void setServiceProvider(ServiceProvider serviceProvider) { this.serviceProvider = serviceProvider; }
-    public ServiceProvider getServiceProvider() { return serviceProvider; }
+
+    public Country() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CountryName getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(CountryName countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public ServiceProvider getServiceProvider() {
+        return serviceProvider;
+    }
+
+    public void setServiceProvider(ServiceProvider serviceProvider) {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
